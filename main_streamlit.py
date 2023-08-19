bb8bad0416410d5d7968456ca44b4c7f6a657a2d
abduyus <35887528+abduyus@users.noreply.github.com>
2023-08-19 09:48:48 +0100

    Addng streamlit

--- main_streamlit.py
+++ main_streamlit.py
@@ -1,9 +1,10 @@
+import streamlit as st
 import cv2
 import time
-from modules.emailing import send_email
 import glob
 import os
 from threading import Thread
+from modules.emailing import send_email
 
 video = cv2.VideoCapture(0)
 time.sleep(1)
@@ -11,6 +12,7 @@ time.sleep(1)
 first_frame = None
 status_list = []
 count = 1
+email_sent = False  # Track whether an email has been sent
 
 def clean_folder():
     print("Clean Folder function started")
@@ -18,6 +20,10 @@ def clean_folder():
     for image in images:
         os.remove(image)
     print("Clean Folder function ended")
+
+st.title("Motion Detection App")
+
+st.write("Live Webcam Feed:")
 
 while True:
     status = 0
@@ -32,7 +38,8 @@ while True:
 
     thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
     dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
+
+    st.image(frame, channels="BGR", use_column_width=True, caption="Live Webcam Feed")
 
     contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
 
@@ -40,8 +47,8 @@ while True:
         if cv2.contourArea(contour) < 5000:
             continue
         x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        if rectangle.any():
+        cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+        if frame.any() and not email_sent:
             status = 1
             cv2.imwrite(f"images/{count}.png", frame)
             count = count + 1
@@ -51,19 +58,17 @@ while True:
 
     status_list.append(status)
     status_list = status_list[-2:]
-    print(status_list)
+    st.write("Status List:", status_list)
 
-    if status_list[0] and status_list[1] == 0:
-        email_thread = Thread(target=send_email, args=(image_with_object, ))
+    if status_list[0] and status_list[1] == 0 and not email_sent:
+        email_thread = Thread(target=send_email, args=(image_with_object,))
         email_thread.daemon = True
         clean_thread = Thread(target=clean_folder)
         clean_thread.daemon = True
         
         email_thread.start()
-        
-
-
-    cv2.imshow("Video", frame)
+        email_sent = True
+        st.write("Email sent!")
 
     key = cv2.waitKey(1)
 
@@ -72,5 +77,3 @@ while True:
 
 clean_thread.start()
 video.release()
-
-
